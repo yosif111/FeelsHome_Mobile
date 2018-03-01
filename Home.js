a1d1575bafe42e067dfea54a034d227c715ce5fb
Yosif Al-Khlefah <435102878@student.ksu.edu.sa>
2018-03-01 21:05:29 +0300

    f

--- Home.js
+++ Home.js
@@ -2,35 +2,49 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, Image, ScrollView, ImageBackground } from 'react-native';
 import { Card, ListItem, Button } from 'react-native-elements'
 
-import styles from './styles';
 const backgroundImage = require('./assets/background.png');
 
 
 class Home extends Component {
 
+    renderLightsCard = () => {
+        return (
+            <View>
+                <Card title="CARD WITH DIVIDER">
+                    {
+                        <Text>Lights</Text>
+                    }
+                </Card>
+            </View>
+        );
+    }
 
     render() {
-        const resizeMode = 'center';
 
         return (
             <View style={{ flex: 1 }}>
-            <ImageBackground style={{ flex: 1 }} source={backgroundImage}>
+                <ImageBackground style={{ flex: 1 }} source={backgroundImage}>
 
-                {/* // <ScrollView>
-                //     <View>
-                //         <Card title="CARD WITH DIVIDER">
-                //             {
-                //                 <Text>Home</Text>
-                //             }
-                //         </Card>
-                //     </View>
-                // </ScrollView> */}
+                    <ScrollView style={styles.mainView}>
+                        {this.renderLightsCard()}
+                        {/* {this.renderTVCard()}
+                        {this.renderAudioCard()}
+                        {this.renderModesCard()} */}
+                    </ScrollView>
 
                 </ImageBackground>
-                </View>
+            </View>
 
         );
     }
 }
+
+const styles = StyleSheet.create({
+    mainView: {
+      flex: 1,
+      marginTop: '20%'
+    },
+
+  });
 
 export default Home;
