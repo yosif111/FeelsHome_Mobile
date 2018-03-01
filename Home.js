036667c4f9e3a2e98c8f7200bb45048dc80ea856
Yosif Al-Khlefah <435102878@student.ksu.edu.sa>
2018-03-01 21:49:06 +0300

    edited home

--- Home.js
+++ Home.js
@@ -1,20 +1,60 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image, ScrollView, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView, ImageBackground, Slider } from 'react-native';
 import { Card, ListItem, Button } from 'react-native-elements'
 
 const backgroundImage = require('./assets/background.png');
+const lightIcon = require('./assets/icon_bulb.png')
 
 
 class Home extends Component {
 
+    state = { RGBvalue: 0 };
+
+    renderLightsSlider = () => {
+        return (
+            <View style={{ flex: 1, alignItems: 'stretch', justifyContent: 'center' }}>
+                <Slider
+                    // style={{height: 10 }}
+                    value={this.state.RGBValue}
+                    thumbTintColor={'rgb(83,45,62)'}
+                    onValueChange={(RGBValue) => this.setState({ RGBValue })}
+                    maximumValue={255}
+                    step={5}
+                    trackImage={require('./assets/color_picker.png')}
+                />
+                <Text>Value: {this.state.RGBValue}</Text>
+            </View>
+        );
+    }
     renderLightsCard = () => {
         return (
             <View>
-                <Card title="CARD WITH DIVIDER">
-                    {
-                        <Text>Lights</Text>
-                    }
+                <Card >
+                    {this.renderCardHeader(lightIcon,'RGB Light')}
+                    {this.renderLightsSlider()}
                 </Card>
+            </View>
+        );
+    }
+
+    renderCardHeader = (icon,label) => {
+        return (
+            <View style={styles.container}>
+            <View
+                style={styles.iconContainer}>
+                <Image
+                    style={styles.icon}
+                    resizeMode="contain"
+                    source={icon} />
+            </View>
+
+            <View
+                style={styles.labelContainer}>
+                <Text
+                    style={styles.label}>
+                    {label}
+                </Text>
+            </View>
             </View>
         );
     }
@@ -41,10 +81,41 @@ class Home extends Component {
 
 const styles = StyleSheet.create({
     mainView: {
-      flex: 1,
-      marginTop: '20%'
+        flex: 1,
+        marginTop: '20%'
+    },
+    cardLabel: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: 'rgb(83,45,62)',
+    },
+    container: {
+        padding : 0,
+        flexDirection: 'row',
+        flex: 1,
     },
 
-  });
+    iconContainer: {
+        flex: 5,
+        marginTop: 14,
+    },
+
+    labelContainer: {
+        padding: 13,
+        flex: 32,
+    },
+
+
+    icon: {
+        height: 21,
+    },
+
+    label: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: 'rgb(83,45,62)',
+    },
+
+});
 
 export default Home;
