7592c85534b7b4f8557a2216ebb8aa562ab7d25d
Ahmed <a7med.ns@gmail.com>
2018-03-02 23:11:39 +0300

    a

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native';
+
+import Wallpaper from '../components/login-components/Wallpaper';
+import Logo from '../components/login-components/Logo';
+import ButtonSubmit from '../components/login-components/ButtonSubmit';
+import UserInput from '../components/login-components/UserInput';
+import RegisterForm from '../components/login-components/RegisterForm';
+
+import emailImg from '../assets/email.png';
+import usernameImg from '../assets/username.png';
+import passwordImg from '../assets/password.png';
+import eyeImg from '../assets/eye_black.png';
 
 class RegisterScreen extends Component {
+    state = { name: '', email: '', password: '' }
+
     static navigationOptions = ({ navigation }) => {
         return {
             title: 'Register'
@@ -10,12 +23,11 @@ class RegisterScreen extends Component {
 
     render() {
         return (
-            <View>
-                <Text>RegisterScreen</Text>
-                <Text>RegisterScreen</Text>
-                <Text>RegisterScreen</Text>
-                <Text>RegisterScreen</Text>
-            </View>
+            <Wallpaper>
+                <Logo />
+                <RegisterForm />
+                <ButtonSubmit onPress={() => this.props.navigation.navigate('login')} />
+            </Wallpaper>
         );
     }
 }
