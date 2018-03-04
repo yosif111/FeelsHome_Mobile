21c6f1f34534d955a95ae0dcddcae1d0fd6c0e0f
Ahmed <a7med.ns@gmail.com>
2018-03-04 22:27:07 +0300

    a

--- components/LightControl.js
+++ components/LightControl.js
@@ -16,6 +16,9 @@ import {
 } from 'react-native';
 import { Card, ListItem, Button, Divider } from 'react-native-elements';
 import CustomSlider from './Common/CustomSlider';
+import axios from 'axios';
+
+const URL = 'http://192.168.8.103:8000';
 
 
 export default class LightControl extends Component {
@@ -23,14 +26,6 @@ export default class LightControl extends Component {
     state = {
         lightsInfo: []
     };
-    constructor() {
-        super();
-
-        this.state = {
-            lightsInfo: []
-        };
-
-    }
 
     componentDidMount() {
         // we should get the lights info and state here
@@ -38,20 +33,60 @@ export default class LightControl extends Component {
             lightsInfo: [
                 {
                     'name': 'Living Room Bulb',
-                    'isOn': true,
-                    'RGBValue': 100
+                    'isOn': false,
+                    'RGBValue': 100,
+                    'id': 1
                 },
                 {
                     'name': 'Bedroom Bulb',
-                    'isOn': true,
-                    'RGBValue': 200
+                    'isOn': false,
+                    'RGBValue': 200,
+                    'id': 2
                 },
                 {
                     'name': 'Kitchen Bulb',
                     'isOn': false,
-                    'RGBValue': 0
+                    'RGBValue': 0,
+                    'id': 3
                 }
             ]
+        });
+    }
+
+    // rgbToHsl(r, g, b) {
+    //     r /= 255, g /= 255, b /= 255;
+
+    //     var max = Math.max(r, g, b), min = Math.min(r, g, b);
+    //     var h, s, l = (max + min) / 2;
+
+    //     if (max == min) {
+    //         h = s = 0; // achromatic
+    //     } else {
+    //         var d = max - min;
+    //         s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+
+    //         switch (max) {
+    //             case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+    //             case g: h = (b - r) / d + 2; break;
+    //             case b: h = (r - g) / d + 4; break;
+    //         }
+
+    //         h /= 6;
+    //     }
+
+    //     return [h, s, l];
+    // }
+
+    onColorChange = (value, lightID) => {
+       axios.post(`${URL}/lights/changeColor`, {
+            'id': lightID,
+            'hue': value
+        })
+        .then(response => {
+            console.log(response);
+        })
+        .catch(error => {
+            console.log(error);
         });
     }
 
