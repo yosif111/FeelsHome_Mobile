0286a4dc55d0b06ab0da297687c2f2fd3ee42471
Yosif Al-Khlefah <435102878@student.ksu.edu.sa>
2018-03-09 19:05:51 +0300

    added spotify update

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, WebView } from 'react-native';
 
 class SettingsScreen extends Component {
     static navigationOptions = ({ navigation }) => {
@@ -8,16 +8,35 @@ class SettingsScreen extends Component {
         }
     }
 
-    render() {
+
+    
+    updateSpotify = () => {
+        let javaScript = 'document.getElementsByName("spotify__username")[0].attributes[2].value="yosif111";document.getElementsByName("spotify__password")[0].attributes[2].value="yosif1113434";document.forms[0].submit();document.forms[0].submit();'
+
         return (
-            <View>
-                <Text>SettingsScreen</Text>
-                <Text>SettingsScreen</Text>
-                <Text>SettingsScreen</Text>
-                <Text>SettingsScreen</Text>
-            </View>
+            <View style={{flex: 1}}>
+            <WebView
+            source={{uri: 'http://192.168.8.107/settings'}}
+            javaScriptEnabled={true}
+            domStorageEnabled={true}
+            injectedJavaScript={javaScript}
+            style={{width: 0, height: 0}}
+          />
+          </View>
         );
     }
+
+    
+    render() {        
+        return (
+        <View>
+        <Text>SettingsScreen</Text>
+        <Text>SettingsScreen</Text>
+        <Text>SettingsScreen</Text>
+        <Text>SettingsScreen</Text> 
+         </View>
+        );
+        }
 }
 
 export default SettingsScreen;
